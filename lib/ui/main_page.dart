f031e0ad03ca15f63e42992bc5379aec28f33a27
Nguyen Duc Quy <quynd@tech.est-rouge.com>
2022-08-26 15:59:15 +0700

    bloc with login page

--- lib/ui/main_page.dart
+++ lib/ui/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_base/app/utils/gen/localized/l10n.dart';
 
 import '../navigation/app_router.gr.dart';
 
